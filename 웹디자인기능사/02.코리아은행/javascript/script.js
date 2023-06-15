e265a3376b1f5e43a746367f0cb8bbb7e86461ec
Yumi Kim <yumii2307@naver.com>
2023-06-15 17:36:43 +0900

    웹디자인기능사

--- 웹디자인기능사/02.코리아은행/javascript/script.js
+++ 웹디자인기능사/02.코리아은행/javascript/script.js
@@ -5,4 +5,30 @@ jQuery(document).ready(function() {
     }).mouseout(function() {
         $('.submenu').stop().slideUp(500);
     });
+
+    setInterval (function() {
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginLeft : -1200});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginLeft : -2400});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginLeft : 0});
+        $('.slidelist').delay(2000);
+    });
+
+    $(function() {
+        $('.tabmenu>li>a').click(function() {
+            $(this).parent().addClass("active")
+            .siblings()
+            .removeClass("active");
+        return false;
+        });
+    });
+
+    $(".notice li:first").click(function() {
+        $("#layer").addClass("active");
+    });
+    $(".btn").click(function() {
+        $("#layer").removeClass("active");
+    });
 });
