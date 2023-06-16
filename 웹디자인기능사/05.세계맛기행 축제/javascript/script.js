3eacfd6a236c6bd0b8ef01d1150c95e9170d55ae
Yumi Kim <yumii2307@naver.com>
2023-06-16 17:44:54 +0900

    웹디자인기능사

---
+++ 웹디자인기능사/05.세계맛기행 축제/javascript/script.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function() {
+    $('.navi>li').mouseover(function() {
+        $(this).find('.submenu').stop().slideDown(500);
+    });
+    $('.navi>li').mouseout(function() {
+        $(this).find('.submenu').stop().slideUp(500);
+    });
+
+    setInterval(function() {
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginLeft: -800});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginLeft: -1600});
+        $('.slidelist').delay(2000);
+        $('.slidelist').animate({marginLeft: 0});
+        $('.slidelist').delay(2000);
+    });
+    
+    $(".notice li:first").click(function() {
+        $("#modal").addClass("active");
+    });
+    $(".btn").click(function() {
+        $("#modal").removeClass("active");
+    });
+});
